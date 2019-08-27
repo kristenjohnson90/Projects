08606019db50cc9c8ed0cb010ba21c0420b7bab4
Kristen Johnson <kristen.menz@gmail.com>
2019-08-27 06:50:16 -0600

    Removed config file

--- config.py
+++
@@ -1,6 +0,0 @@
-client_id = 'zyfnnrx'
-client_secret = 'jmlwI9ipmJO!@2pMrwEGSqmslx$t6pqobs2SGr$w5xuePU2ErJOwK23N$wfIx3hO'
-token_url = 'https://api.elliemae.com/oauth2/v1/token'
-username = 'kjohnson@encompass:BE903356'
-password = 'August@2019'
-
