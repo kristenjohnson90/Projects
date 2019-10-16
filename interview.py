4cad716b2af5fbc01fca88956ddf13b067420db8
Kristen Johnson <kristen.menz@gmail.com>
2019-10-16 10:18:48 -0600

    Delete interview.py

--- interview.py
+++
@@ -1,9 +0,0 @@
-# between 1 and 1000000
-# digits add up to 42
-
-count = 0
-for i in range(1,1000001):
-    num = sum(list(map(int, str(i))))
-    if num == 42:
-        count = count + 1
-print(count)
